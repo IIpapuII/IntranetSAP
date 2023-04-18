eec429c1dc58e32845182cbf4f8d3e8ff1991c70
IIpapuII <wnserrano@misena.edu.co>
2023-04-18 16:29:55 -0500

    asignación de procesos con la estructura de rutas globales.

--- config.py
+++ config.py
@@ -8,4 +8,17 @@ load_dotenv()
 class Config():
     """Global configuration module"""
     SERVER_NAME_ONE = os.getenv("hostdb")
+    DATABASE_ONE = os.getenv("database")
+    USERDB_ONE = os.getenv("userDB")
+    PASSWOR_One= os.getenv("passwordDB")
+
+    SERVER_SAP = os.getenv("SERVERSAP")
+    USER_SAP = os.getenv("USERSAP")
+    PASSWORD_SAP = os.getenv("PASSWORD_SAP")
+    PORT_SAP = os.getenv("PORTSAP")
+    USER_MAIL = os.getenv("USERMAIL")
+    PASSWORD_MAIL = os.getenv("PASSWORDEMAIL")
     DEBUG = True
+
+    TEMPLATE_FOLDER = "views/templates/"
+    STATIC_FOLDER = "views/static"
